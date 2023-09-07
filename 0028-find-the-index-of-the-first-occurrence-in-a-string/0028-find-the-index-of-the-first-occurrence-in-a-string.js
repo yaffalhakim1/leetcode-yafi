788267559191e90933e4e615c0e8bbf0f8707499
Muhammad Yafi Alhakim <yafialhakim64@gmail.com>
2023-09-07 10:16:30 +0700

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.js

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -35,14 +35,27 @@ var strStr = function(haystack, needle) {
     
 };
 
-// Kami menghitung panjang string haystack dan panjang string needle.
+//fungsi strStr yang digunakan untuk mencari indeks pertama di mana substring needle ditemukan dalam string haystack
 
-// Dalam kasus khusus, jika panjang needle adalah 0, maka kita tahu bahwa needle ditemukan di posisi 0 dalam haystack, dan kita mengembalikan 0.
+//Ide utama dari solusi ini adalah dengan menggunakan dua loop bersarang untuk mencari substring needle dalam string haystack. Loop pertama me-loop melalui string haystack, dan loop kedua me-loop melalui karakter-karakter needle untuk mencocokkannya dengan karakter-karakter dalam haystack.
 
-// Kami melakukan loop melalui string haystack dengan menggunakan variabel i. Kami hanya perlu melanjutkan hingga hLen - nLen karena jika panjang needle lebih besar dari sisa panjang haystack, maka tidak mungkin ada hasil.
+// //LANGKAH
+// Dapatkan panjang string haystack (hLen) dan panjang string needle (nLen).
+// Jika panjang needle (nLen) adalah 0, maka kembalikan 0, karena needle kosong dan ditemukan di indeks 0.
+// Lakukan loop melalui string haystack hingga (hLen - nLen) untuk menghindari pencarian yang tidak perlu karena panjang needle. Variabel i akan digunakan sebagai indeks awal pencarian.
+// Di dalam loop utama, inisialisasi variabel j dengan 0. Variabel ini akan digunakan untuk membandingkan karakter needle dengan karakter haystack.
+// Dalam loop kedua (while), cocokkan karakter needle[j] dengan karakter haystack[i + j]. Selama karakter cocok dan j kurang dari panjang needle (nLen), tingkatkan nilai j.
+// Jika loop kedua keluar karena semua karakter cocok, yaitu j sama dengan panjang needle (nLen), maka kita telah menemukan needle dalam haystack. Kembalikan indeks i.
+// Jika loop utama selesai dan tidak ada pencocokan yang ditemukan, kembalikan -1 untuk menunjukkan bahwa needle tidak ditemukan dalam haystack.
 
-// Dalam loop, kami menggunakan variabel j untuk mencocokkan karakter needle dengan karakter haystack. Kami terus menambahkan j selama karakter cocok.
+// ILUSTRASI
+//  haystack adalah "hello world" dan needle adalah "world"
 
-// Jika semua karakter cocok (yaitu, j mencapai panjang needle), kami mengembalikan indeks i sebagai hasil, yang menunjukkan di mana needle ditemukan dalam haystack.
-
-// Jika loop selesai dan tidak ditemukan, kami mengembalikan -1.
+// Panjang haystack (hLen) adalah 11 dan panjang needle (nLen) adalah 5.
+// Loop pertama dimulai dari i = 0 hingga i = 6 (11 - 5).
+// Pada setiap iterasi loop pertama, kita memulai loop kedua (while) untuk membandingkan karakter:
+// Ketika i = 0, kita membandingkan "world" dengan "hello", tidak cocok.
+// Ketika i = 1, kita membandingkan "world" dengan "ello ", tidak cocok.
+// Ini berlanjut hingga i = 6, di mana kita membandingkan "world" dengan "world", semua karakter cocok.
+// Oleh karena itu, kita menemukan needle pada haystack di indeks 6.
+// Fungsi mengembalikan nilai 6 sebagai hasil.
