7e185aff923a13d2b52b980bb4f568c34b554b68
Muhammad Yafi Alhakim <yafialhakim64@gmail.com>
2023-09-07 10:12:43 +0700

    Update 0027-remove-element.js

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -24,11 +24,25 @@ var removeElement = function(nums, val) {
 
 //removeElement digunakan untuk menghapus semua kemunculan nilai tertentu `val` dari array `nums` dan mengembalikan panjang dari array baru setelah penghapusan. selain menghapus nilai, urutan dalam array jg harus di perttahankan.
 
+//Ide utama dari solusi ini adalah untuk menggeser elemen-elemen yang bukan val ke posisi awal array dan menghitung panjang array baru setelah elemen tersebut dihapus.
 
-// gunakan 2 variabel i dan j untuk melacak posisi elemen dalam array
-// loop sepanjang nums untuk memeriksa setiap elemen dari nums
-// jika elemen dari array saat ini `nums[j]` tidak sama dengan `val` (nilai yang ingin dihapus) maka salin elemen `nums[j]` ke posisi valid atau baru `nums[i]` kemudian tingkatkan indeks `i` untuk menunjuk ke posisi berikutnya
-// dengan ini, kita pastikan bahwa elemen yang sama dengan val dihapus dari array
+//LANGKAH
+// Inisialisasi sebuah variabel i dengan nilai 0. Variabel ini akan digunakan sebagai penunjuk untuk menentukan posisi di mana elemen yang tidak sama dengan val akan disalin ke dalam array.
+// Loop melalui array nums dengan menggunakan variabel j sebagai penunjuk.
+// Dalam setiap iterasi, periksa apakah elemen saat ini (nums[j]) tidak sama dengan val.
+// Jika elemen saat ini tidak sama dengan val, maka kita akan menyalin elemen tersebut ke dalam array pada posisi i (posisi baru).
+// Tingkatkan nilai i dengan 1 setelah menyalin elemen.
+// Lanjutkan proses ini hingga selesai melalui seluruh array nums.
+// Setelah loop selesai, nilai i akan mewakili panjang array baru yang telah dihasilkan setelah menghapus semua kemunculan elemen dengan nilai val.
+// Kembalikan nilai i sebagai hasil, yang juga merupakan panjang array baru.
 
+//ILUSTRASI
+// nums = [3, 2, 2, 3, 4, 5]
 
+// Pada awalnya, i diatur ke 0.
+// Ketika loop dimulai, elemen pertama adalah 3, yang sama dengan val. Oleh karena itu, tidak ada yang perlu dilakukan dalam langkah ini.
+// Kemudian, elemen kedua adalah 2, yang tidak sama dengan val. Kita akan menyalin elemen ini ke dalam array pada posisi i, yaitu posisi 0, dan kemudian kita tingkatkan nilai i menjadi 1.
+// Proses ini berlanjut untuk elemen-elemen selanjutnya, dan kita akan menyalin elemen-elemen yang tidak sama dengan val ke dalam posisi yang sesuai.
+// Setelah selesai melalui seluruh array, nilai i adalah 3, yang berarti ada 3 elemen yang tidak sama dengan val.
+// Hasilnya adalah array nums yang telah dimodifikasi menjadi [2, 2, 4, 5], dan panjang array baru adalah 3.
 
